1de8a6edd34286013e6d7611fe396fa73a47e7f8
Yusuke Naito <yuk@arcstyle.jp>
2013-09-09 19:22:50 +0900

    コマンドライン引数を使用するように修正

--- src/FizzBuzz.groovy
+++ src/FizzBuzz.groovy
@@ -1,15 +1,19 @@
-println 'hello'
-
-def max = 100;
-
-for (def current = 0; current < max; current++) {
-    if (current % 15 == 0) {
-        println 'Fizz Buzz';
-    } else if (current % 3 == 0) {
-        println 'Fizz';
-    } else if (current % 5 == 0) {
-        println 'Buzz';
-    } else {
-        println current;
+public class FizzBuzzRunner
+{
+    public void run(max) {
+        for (def current = 1; current <= max; current++) {
+            if (current % 15 == 0) {
+                println 'Fizz Buzz';
+            } else if (current % 3 == 0) {
+                println 'Fizz';
+            } else if (current % 5 == 0) {
+                println 'Buzz';
+            } else {
+                println current;
+            }
+        }    
     }
 }
+
+def fbr = new FizzBuzzRunner();
+fbr.run(args[0].toInteger());
